11f7162eb2e7b5a1ea74b3cb03ea8a29e2f7d2f0
Dmytro Kovalchuk <d.kovalchuk@levi9.com>
2016-06-30 10:20:42 +0300

    Initial commit

---
+++ src/main/java/snooker/SnookerLeague.java
@@ -0,0 +1,15 @@
+package snooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starter class for Snooker League application.
+ */
+@SpringBootApplication
+public class SnookerLeague {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnookerLeague.class, args);
+    }
+}
